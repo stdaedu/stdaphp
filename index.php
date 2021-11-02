93baaf898401f753078cbaf28482192fc9b9da4d
boldoo <burnaa81@gmail.com>
2021-11-02 14:14:43 +0800

    Lesson3

--- index.php
+++ index.php
@@ -1,45 +1,24 @@
 <?php
     include('dbconnect.php');
-    // $name = mysqli_real_escape_string($connection, $_POST['username']);
-    // $email = mysqli_real_escape_string($connection, $_POST['email']);
-    // $password = mysqli_real_escape_string($connection, $_POST['password']);
-    // $cpassword = mysqli_real_escape_string($connection, $_POST['cpassword']);
-
-    // $query = "INSERT INTO users (username, email, password) values ('$name', '$email', '$password')";
-    // $name = $_POST['username'];
-
-    // $post = array();
-
-    // $post['username'] = 'john';
-
-    // $post['email'] = 'john@mail.com';
-    // echo $post['username'] . '<br>' . $post['email'];
 
     $error = array();
-    // hooson baigaa esehiig shalgah
-    if(isset($_POST['username'])){
-        
-    } else {
-        $error['empty_username'] = "Tanii username hooson bn";
-    }
+
+    $name = mysqli_real_escape_string($connection, $_POST['username']);
+    $email = mysqli_real_escape_string($connection, $_POST['email']);
+    $password = mysqli_real_escape_string($connection, $_POST['password']);
+    $cpassword = mysqli_real_escape_string($connection, $_POST['cpassword']);
     
-    if(isset($_POST['email'])){
-        
+    // hooson baigaa talbariig shalgah
+    if(!empty($name) && !empty($email) && !empty($password) && !empty($cpassword)){
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            echo "email mon baina";
+        } else {
+            $error['email_type_wrong'] = "Email buruu bn";
+        }
     } else {
-        $error['empty_email'] = "Tanii email hooson bn";
+        $error['empty'] = "Hooson talbar uldeej bolohgui!";
     }
 
-    if(isset($_POST['password'])){
-        
-    } else {
-        $error['empty_password'] = "Tanii password hooson bn";
-    }
-
-    if(isset($_POST['cpassword'])){
-        
-    } else {
-        $error['empty_cpassword'] = "Tanii repeat password hooson bn";
-    }
     // aldaanuudiig hevlej haruulah
     if(count($error) >= 1){
         foreach($error as $showerror){
@@ -57,11 +36,11 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="index.php" method="POST">
-        <input type="text" name="username" placeholder="Username">
-        <input type="text" name="email" placeholder="Email">
-        <input type="password" name= "password" placeholder="Password">
-        <input type="password" name= "cpassword" placeholder="Repeat password">
+    <form action="index.php" method="POST" autocomplete="">
+        <input type="text" name="username" placeholder="Username" required value="<?php echo $name ?>">
+        <input type="text" name="email" placeholder="Email" required value="<?php echo $name ?>">
+        <input type="password" name= "password" placeholder="Password" requierd>
+        <input type="password" name= "cpassword" placeholder="Repeat password" required>
         <input type="submit" name="submit" value="Signup">
     </form>
     <?php 
